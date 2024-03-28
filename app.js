5a706711e1418f7e19de6887202d5a691aeb7107
kamyabmomin <kamyab.momin.2024@gmail.com>
2024-03-28 11:03:40 +0530

    daynemic component page is add

--- app.js
+++ app.js
@@ -1306,6 +1306,40 @@ app.get("/timeconvertor", async (req, res) => {
 
 
 
+
+app.get("/component", async (req, res) => {
+    res.status(200)
+    try {
+
+        var comboname = req.query.name;
+        console.log(comboname);
+
+        alldata1 = 1;
+        if (req.query.name) {
+            var sql = `select  select_master.select_name ,select_master.inputtype,option_master.option_name ,option_master.option_value ,option_master.option_id ,option_master.selectedoption  from select_master inner JOIN option_master ON select_master.select_id  = option_master.select_id   where  select_master.select_name ='${comboname}' `
+
+        }
+        else {
+            alldata1 = 0
+        }
+        console.log(alldata1)
+        if (alldata1 == 0) {
+            res.render("component", { alldata1 });
+        } else {
+            var [result] = await con.query(sql)
+            console.log(alldata1)
+            console.log(result)
+            res.render("component", { alldata: result, alldata1, comboname });
+        }
+    
+    }
+    catch (e) {
+        console.log("error in syntex")
+    }
+})
+
+
+
 app.listen(port, (error) => {
     if (!error) {
         console.log("server is running")
