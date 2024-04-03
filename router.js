4d528366e50f21a42db59b51e451ef4281b9f8b3
kamyabmomin <kamyab.momin.2024@gmail.com>
2024-04-03 12:45:28 +0530

    form using ajx and fetch api task route and controller crated

--- router.js
+++ router.js
@@ -2,6 +2,7 @@ const express = require("express")
 const { homePage, registerData, keyCompare, login, loginData, forgetPassword, forgetPasswordData } = require("./controller/login_register/homepage");
 const { validation1 } = require("./tokenvalidation");
 const { writeData, more } = require("./controller/crude_id_file");
+const { save, allList, deleteId, updateId, updateIdPost, city } = require("./controller/employe_form");
 const router = express.Router()
 
 
@@ -43,5 +44,37 @@ router.route("/crudinfile").get(validation1, (req, res) => {
 
 router.route("/details").post(validation1, writeData)
 router.route("/more").post(validation1, more)
+
+
+//employeForm
+router.route("/employeformvalidation").get(validation1, (req, res) => {
+    res.status(200);
+    res.render("employe_form/form")
+})
+
+router.route("/ajxform").get(validation1, (req, res) => {
+    let educationvalid = true;
+    let valid = true;
+    var data = ""
+    res.render('employe_form/ajxform', { valid, educationvalid, data })
+})
+
+router.route("/save").post(validation1,save);
+router.route("/alllist").get(validation1, allList)
+router.route("/delete/id").get(validation1, deleteId);
+router.route("/update/id").get(validation1, updateId);
+router.route("/update/id").post(validation1, updateIdPost);
+router.route("/city/city/city").post(validation1 ,city)
+
+
+//fetch_api 
+router.route("/tablefetchapi").get(validation1, (req, res) => {
+    res.render('fetch_api/tablefetchapi')
+})
+
+router.route("/details").get( validation1, (req, res) => {
+    res.render('fetch_api/detailstablefetchapi')
+})
+
 
 module.exports = router;
